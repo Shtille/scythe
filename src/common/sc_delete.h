4a07db6398c9e9a182d0cde7e3df4dc211a2e7ba
Vladimir <v.shtille@gmail.com>
2018-12-30 13:19:25 +0300

    added safe delete macros

---
+++ src/common/sc_delete.h
@@ -0,0 +1,7 @@
+#ifndef __SCYTHE_DELETE_H__
+#define __SCYTHE_DELETE_H__
+
+#define SC_SAFE_DELETE(x) if (x) { delete x; x = nullptr; }
+#define SC_SAFE_DELETE_ARRAY(x) if (x) { delete[] x; x = nullptr; }
+
+#endif
